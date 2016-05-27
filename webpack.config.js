efc4b656c61b56f6d37e531ef0df9f00dcc0859c
Katherine <nebel20@yandex.ru>
2016-05-27 19:38:49 +0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+
+const PATHS = {
+    app: path.join(__dirname, 'src'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    entry: [
+        PATHS.app
+    ],
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'react-hot!babel'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
